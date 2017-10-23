102e74e9fb48205e3e12a4d7b5b2bbb8a2ee8128
Kosmos <ZeroProject@foxmail.com>
2017-10-23 15:11:20 +0800

    Glide整理

---
+++ app/src/main/java/widget/small/com/smallwidget/tools/glide/transformations/TransformRotate.java
@@ -0,0 +1,36 @@
+package widget.small.com.smallwidget.tools.glide.transformations;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
+/**
+ * Glide旋转图片
+ * Created by leichao on 2016/4/22.
+ */
+public class TransformRotate extends BitmapTransformation {
+    private float rotateRotationAngle = 0f;
+
+    public TransformRotate(Context context, float rotateRotationAngle) {
+        super( context );
+
+        this.rotateRotationAngle = rotateRotationAngle;
+    }
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+        Matrix matrix = new Matrix();
+
+        matrix.postRotate(rotateRotationAngle);
+
+        return Bitmap.createBitmap(toTransform, 0, 0, toTransform.getWidth(), toTransform.getHeight(), matrix, true);
+    }
+
+    @Override
+    public String getId() {
+        return "rotate" + rotateRotationAngle;
+    }
+}
